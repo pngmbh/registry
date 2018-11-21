207dd135f735d4b117ec5e81963cbfea2e1a9481
Richard Wossal <richard@r-wos.org>
2018-11-21 12:52:06 +0100

    Revert "fix(registry): disable storage redirects"
    This reverts commit e4476238a4fc836d1024f248c258d5c790c77533.

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ const (
 func main() {
 	log.Println("INFO: Starting registry...")
 	storageType := getenv("REGISTRY_STORAGE", "filesystem")
-	os.Setenv("REGISTRY_STORAGE_REDIRECT_DISABLE", "true")
 	if storageType == "gcs" {
 		log.Println("INFO: using google cloud storage as the backend")
 		if _, err := os.Stat("/var/run/secrets/deis/registry/creds/key.json"); err != nil {
